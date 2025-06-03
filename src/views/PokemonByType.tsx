00d3a50eef1e3598f8954b4003a03cb999bc5d07
Inés Togni <ines.togni@gmail.com>
2025-06-03 13:43:33 +0200

    Pokemon By Type (#3)
    Added functionality to filters pokemons by type.

---
+++ src/views/PokemonByType.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { PokemonListByType } from "../components/PokemonListByType";
+
+const PokemonByType: React.FC = () => <PokemonListByType />;
+
+export default PokemonByType;
