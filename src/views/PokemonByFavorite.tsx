8329ba696b774eb2bae357dc165a96132988eab0
Inés Togni <ines.togni@gmail.com>
2025-06-04 10:57:19 +0200

    Favorite View and Navigation (#4)
    Added Favorite View and Navigation

---
+++ src/views/PokemonByFavorite.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { FavoritePokemon } from "../components/FavoritePokemon";
+
+const PokemonByFavorite: React.FC = () => <FavoritePokemon />;
+
+export default PokemonByFavorite;
